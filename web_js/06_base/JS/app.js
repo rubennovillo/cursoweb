c17cbfb7a603e19bc3aed479562d665f65598ca5
ruben <sonikeys@gmail.com>
2019-07-22 21:03:05 +0200

    cronometro

--- web_js/06_base/JS/app.js
+++ web_js/06_base/JS/app.js
@@ -1,34 +1,34 @@
+import { Crono } from "./crono.js";
 
-export function app () {
-      console.log('Cargada app')
-      let btnSalir = document.querySelector('#btn-salir')
-      let out = document.querySelector('#output')
-      let btnCancelar
-      let handler
+export function app() {
+    console.log('Cargada app') 
+    
+    let btnSalir = document.querySelector('#btn-salir')
+    let dlgCancelar = document.querySelector('#dlg-cancelar')
+    let btnCancelar = document.querySelector('#btn-cancelar')
+    let dlgTime = document.querySelector("#dlg-time")
+    let handlerSalir
+    let handlerInterval
 
-      btnSalir.addEventListener('click', onClick)
+    btnSalir.addEventListener('click', onClick)
+    btnCancelar.addEventListener('click', onCancel)
 
-      function onClick() {
-            let url = 'https://www.google.com'
-            let message = `
-            <section>
-            <p>redireccionando ...</p>
-            <button id = "btn-cancelar">Cancelar</button>
-            </section>
-            `
-            handler = setTimeout (() => {
-                  location.assign(url)
-            }, 2000)
-            out.innerHTML = message
-            btnCancelar = document.querySelector('#btn-cancelar')
-            btnCancelar.addEventListener('click', onCancel)
-      }
-      
-      function onCancel () {
-            clearTimeout(handler)
-            out.innerHTML = message
-            
-      }
-      }
+    new Crono()
 
-      
+    function onClick() {
+        let url = 'https://www.google.com/'
+        handlerSalir = setTimeout(() => location.assign(url),
+                    4800)
+        dlgCancelar.showModal()
+        dlgTime.value = 5
+        handlerInterval = setInterval( () => {
+            dlgTime.value -= 1
+        }, 1000)
+    }
+
+    function onCancel() {
+        clearTimeout(handlerSalir)
+        clearInterval(handlerInterval)
+        dlgCancelar.close()
+    }
+ }
