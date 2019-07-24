0771b3de647223db75e07698dba90319dcf92439
ruben <sonikeys@gmail.com>
2019-07-24 21:23:15 +0200

    nodos

--- web_js/10_base/JS/app.js
+++ web_js/10_base/JS/app.js
@@ -7,52 +7,7 @@
 
 export function app() {
       console.log('cargada app')
-      console.log(document)
-      console.dir(document)
 
-      const aBtns = document.querySelectorAll('.btn')
-      /* const btn = document.querySelector('#btn')
-      const btn_no = document.querySelector('#btn_no') */
-      const inNombre = document.querySelector('#in-nombre')
-      const out1 = document.querySelector('#out1')
-      const inNombre2 = document.querySelector('#in-nombre2')
-      const out2 = document.querySelector('#out2')
-      /*document.querySelector('#btn').onclick = saludar
-      document.querySelector('#btn_no').onclick = protestar*/
-      aBtns.forEach( btn => 
-            btn.addEventListener('click', onBtnClick))
+      boton = document.querySelector('button')
       
-      
-      inNombre2.addEventListener('input', escribir)
-
-      function onBtnClick(ev) {
-            let message
-            
-            switch (ev.target.id) {
-                  case 'btn':
-                        message = `Hola ${inNombre.value}, buenas tardes`
-                        break;
-            
-                  case 'btn_no':
-                        message = `¿Por que tocas ${inNombre.value}?`
-                        break;
-                  default:
-                        message = 'Boton sin funcionalidad'
-            } out1.value = message
-      }
-      function escribir() {
-      out2.value = 
-      `${inNombre2.value}`
-      } 
 }
-
-     /* function saludar() {
-      out1.textContent =
-      
-
-      }
-      function protestar() {
-      out1.value = 
-      `¿Por que tocas ${inNombre.value}?`
-      }  
-*/
--- web_js/9a_enlaces/JS/app.js
+++ web_js/9a_enlaces/JS/app.js
@@ -8,8 +8,25 @@
 export function app() {
       console.log('cargada app')
 
-      boton = document.querySelector ('button')
+            const alinks = document.querySelectorAll('a')
+
+            alinks.forEach(enlace => enlace
+                  .addEventListener('click', onClickEnlace))
+
+            
       
+      function onClickEnlace(ev) {
+            ev.target.outerHTML = "jajajajajja"
+      }
 
 }
-      
+      //Creacion de nodos
+
+      let origen =document.querySelector('.addNodos div')
+      let txtHTML = '<p>Este parrafo esta construido en html</p>'
+      origen.innerHTML = txtHTML
+
+      let content = document.createTextNode('creade desde JS')
+      html.appendChild(content)
+      let html = document.createElement ('p')
+      console.log(html)
