9e62ec8019ffb053ba08bd02b3769a0ad3713f25
ruben <sonikeys@gmail.com>
2019-09-06 01:31:45 +0200

    desde casa

--- web_js/22_paises/js/app.js
+++ web_js/22_paises/js/app.js
@@ -1,27 +1,40 @@
-      
-      let url = "https://restcountries.eu/rest/v2/all"
-      fetch(url)
-      .then(res => res.json())
+// Nodos del DOM
+let regionsList = document.querySelector("#continente")
+let countriesList = document.querySelector("#pais")
+
+
+// Asociación de manejadores de eventos
+
+regionsList.addEventListener("change", newRegionSelection)
+countriesList.addEventListener("change", newCountrySelection)
+
+ // Funciones manejadoras de eventos
+function newRegionSelection(ev) {
+  
+}
+
+function newCountrySelection(ev) {
+  CountryInfo(ev.target.value)
+}
+
+fetch("https://restcountries.eu/rest/v2/all")
+.then(res => res.json())
 .then(data => init(data))
-.catch(err => console.log("Error:", err));
+.catch(err => console.log("Error:", err))
 
 function init(countriesData) {
-  countries = countriesData;
-  let options = "";
+  countries = countriesData
+  let options = ""
+ 
   countries.forEach(country => options+=`<option value="${country.alpha3Code}">${country.name}</option>`)
 
-  countriesList.innerHTML = options;
-  countriesList.selectedIndex = Math.floor(Math.random()*countriesList.length)
-  displayCountryInfo(countriesList[countriesList.selectedIndex].value)
+  countriesList.innerHTML = options
 }
-function displayCountryInfo(countryByAlpha3Code) {
-      const countryData = countries.find(country => country.alpha3Code === countryByAlpha3Code);
-      document.querySelector("#flag-container img").src = countryData.flag;
-      document.querySelector("#flag-container img").alt = `Flag of ${countryData.name}`;  
-      document.getElementById("capital").innerHTML = countryData.capital;
-      document.getElementById("dialing-code").innerHTML = `+${countryData.callingCodes[0]}`;
-      document.getElementById("population").innerHTML = countryData.population.toLocaleString("en-US");
-      document.getElementById("currencies").innerHTML = countryData.currencies.filter(c => c.name).map(c => `${c.name} (${c.code})`).join(", ");
-      document.getElementById("region").innerHTML = countryData.region;
-      document.getElementById("subregion").innerHTML = countryData.subregion;
-    }
+
+function CountryInfo(countryInfo) {
+  let countryData = countries.find(country => country.alpha3Code == countryInfo)
+  document.querySelector("#flag-container img").src = countryData.flag;
+  document.getElementById("capital").innerHTML = countryData.capital
+  document.getElementById("region").innerHTML = countryData.region
+  document.getElementById("subregion").innerHTML = countryData.subregion
+}
